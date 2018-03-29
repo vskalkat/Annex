de41765622a562b180ad01fc3585155d53c979e8
Saif Khan <>
2018-03-29 15:52:22 -0400

    Port over server code

--- server/app.js
+++ server/app.js
@@ -10,40 +10,211 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
+var app = express();
+
+var bcrypt = require('bcrypt');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'pass',
+  database : 'my_db'
+});
+
+createSchema(connection);
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use("", express.static(__dirname + ""));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.get('/ ', (req, res) => { //anonymous function
+  console.log("GET request received for root");
+  res.sendFile(__dirname + '../public/loginView.html');
+})
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/calculator', (req, res) => { //anonymous function
+  console.log("GET request received for calculator page");
+  res.sendFile(__dirname + '../public/calculator.html');
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+const saltRounds = 10;
 
-const server = app.listen((process.env.PORT || 3000), () => {
-  const port = server.address().port;
-  console.log('Node.js server running at localhost:', port);
-});
+function addUser(user, connection) {
+    var sql = "INSERT INTO users (email, password, is_premium, fav_teacher) VALUES ('"+ user.email + "', '" + user.password + "', " + user.is_premium + ", '" + user.fav_teacher + "');";
+    connection.query(sql, function (err, result) {
+      console.log("Banchot added " + result + " result : " + sql + " with err " + err);
+    });
+}
+
+function retrieveUser(email, connection, listener) {
+    var sql = "SELECT * FROM users WHERE email = '" + email + "'";
+    connection.query(sql, function (err, result) {
+
+      if(err) {
+        listener(false);
+        console.log("errored out " + err);
+        return;
+      } else {
+         listener(result);
+         console.log("User successfully retreived " + result + " with err " + err + " where query was " + sql);
+      }
+
+
+    });
+}
+
+function createDefaultUsers(connection) {
+    var sql = "INSERT INTO users (email, password, is_premium) VALUES ('banchot@hotmail.com', 'password', true, 'igor');";
+    connection.query(sql, function (err, result) {
+      console.log("Banchot added " + result);
+    });
+}
+
+function createSchema(con) {
+      con.query("CREATE DATABASE IF NOT EXISTS my_db", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+      });
+
+      var sql = "CREATE TABLE IF NOT EXISTS users (email VARCHAR(255) PRIMARY KEY, password VARCHAR(255), is_premium BOOLEAN, fav_teacher VARCHAR(255))";
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("User Table created");
+      });
+
+      sql = "CREATE TABLE IF NOT EXISTS expressions (id INT PRIMARY KEY AUTO_INCREMENT, user_email VARCHAR(255), input_exp VARCHAR(255) UNIQUE, simplified_exp VARCHAR(255), steps VARCHAR(255))";
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Expression Table created");
+      });
+
+      createDefaultUsers(con);
+}
+
+app.post('/login', function (req, res) {
+  var email = req.body.userCredentials.username;
+  var password = req.body.userCredentials.password;
+
+  retrieveUser(email, connection, function(result) {
+      if(result && result.length > 0) {
+          user = result[0];
+          console.log(user);
+         
+
+          bcrypt.compare(password, user.password, function(err, response) {
+              if(response) {
+                const token = jwt.sign({}, 'my_secret_key', {expiresIn: '60000'});
+                var contentToSend = {
+                  "token" : token
+                };
+
+                res.send(JSON.stringify(contentToSend));
+                console.log("login succeeded");
+              } else {
+                var contentToSend = {"message" : "login failed, wrong password"};
+                res.send(JSON.stringify(contentToSend));
+              }
+
+          });
+      }
+      else {
+          var contentToSend = {"message" : "login failed, no user."};
+          res.send(JSON.stringify(contentToSend));
+      }
+  });
+
+})
+
+//do stuff that requires authentication privlidges here
+app.get('/protected', ensureToken, function(req, res){
+  var tokenVerified = false;
+  jwt.verify(req.headers["authorization"], 'my_secret_key', function(err, data){
+    if(err){
+      console.log("Error in token verification:" + err);
+      tokenVerified = false;
+      res.sendStatus(403);
+    } else {
+      console.log('token verification: SUCCESS.');
+      tokenVerified = true;
+      res.json({
+        tokenVerified : tokenVerified
+      });
+    }
+  })
+})
+
+function ensureToken(req, res, next){
+  console.log("ensuring token...");
+  const bearerHeader = req.headers["authorization"];
+  if(typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+app.post('/signUp', function (req, res) {
+  var email = req.body.userCredentials.email;
+  var password = req.body.userCredentials.password;
+  var isPremiumRegistration = req.body.userCredentials.isPremiumRegistration;
+  var fav_teacher = req.body.userCredentials.fav_teacher;
+
+  var user = {
+    email: email,
+    password: password,
+    is_premium: isPremiumRegistration,
+    fav_teacher: fav_teacher
+  };
+
+  if (isValidEmail(email)){
+    const token = jwt.sign({}, 'my_secret_key', {expiresIn: '60000'});
+    var contentToSend = {
+      "token" : token
+    };
+    res.send(JSON.stringify(contentToSend)); // this is a 200
+    console.log("SignUp POST request hit! 1 success ");
+
+    //add new user to database
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      bcrypt.hash(password, salt, function(err, hash) {
+
+          bcrypt.hash(fav_teacher, salt, function(err, fav_enc) {
+            user.password = hash;
+            user.fav_teacher = fav_enc;
+            addUser(user, connection);
+          });
+      });
+    });
+
+  } else {
+    res.sendStatus(403);
+  }
+
+})
+
+
+function isValidEmail(email){
+  return (email && email.length > 0);
+}
+
+var server = app.listen(8042, function(){
+  var port = server.address().port
+  console.log('Node.js server running at localhost:%s', port)
+})
+
+function validateCredentials(user, password){
+
+  if(!user) return false;
+
+  bcrypt.compare(password, user.password, function(err, res) {
+      return res
+  });
+}
--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+           // <meta name="description" content="Simplify boolean expressions and see the steps.">
+           // <meta name="author" content="Saif Khan - Vik Kalakt">
+
+           // <title>Boolean Sign Up</title>
+
+           // <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+           // <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+           // <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+           // <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+           // <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"/>
+           // <script src="loginView.js"></script>
+           //<meta charset="utf-8">
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+           
+
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
