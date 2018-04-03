b1431b8af8c1f013ecf9bdbb35f8e32efa2c4a0c
Vik Kalkat <vikramjit@clearbridgemobile.com>
2018-04-03 03:11:35 -0400

    updated manage profiles page

--- public/manageProfileView.js
+++ public/manageProfileView.js
@@ -1,14 +1,79 @@
 $(document).ready(function(){
 
 
-  $('.fa-minus-circle').click(function(){
-    $(this).parent().remove();
+  $('.fa-minus-circle').on('click', function(){
+    $(this).parent().fadeTo( "normal", 0, function(){
+      $(this).remove();
+    });
   });
+
 
   $('.addSkillBtn').click(function(){
     var newSkillText = $('#addSkillField').val();
-    $('.mySkillsList').append("<li class='list-group-item secondaryText'>"+ newSkillText +"<i class='fa fa-minus-circle myIcon'></i></li>");
+    $.trim(newSkillText);
+    if(newSkillText != "") {
+      $('.mySkillsList').append("<li class='list-group-item secondaryText'>"+ newSkillText +"<i class='fa fa-minus-circle myIcon'></i></li>");
+    }
+    $('.fa-minus-circle').on('click', function(){
+      $(this).parent().fadeTo( "normal", 0, function(){
+        $(this).remove();
+      });
+    });
+    $('#addSkillField').val('');
+  });
 
+
+  $('.newProjectButton').click(function(){
+    $('.newProjectBox').toggleClass('collapse');
+  });
+
+
+  //Create project
+  $("#submitBtn").click(function(){
+    console.log("submitBtn button clicked!" );
+
+    var projectName = $("#projectName").val();
+    var projectDescription = $("#projectDescription").val();
+    var programSelect = $("#programSelect option:selected").text();
+
+    var softwareSkill = $('#softwareSkill').prop( "checked" );
+    var firmwareSkills = $('#firmwareSkills').prop( "checked" );
+    var mechanicalSkills = $('#mechanicalSkills').prop( "checked" );
+    var electricalSkills = $('#electricalSkills').prop( "checked" );
+    var dataSkills = $('#dataSkills').prop( "checked" );
+    var designSkills = $('#designSkills').prop( "checked" );
+
+
+    project = {
+      "projectName" : projectName,
+      "projectDescription" : projectDescription,
+      "programSelect" : programSelect,
+      "softwareSkill" : softwareSkill,
+      "firmwareSkills" : firmwareSkills,
+      "mechanicalSkills" : mechanicalSkills,
+      "electricalSkills" : electricalSkills,
+      "dataSkills" : dataSkills,
+      "designSkills" : designSkills
+    };
+
+    console.log(project);
+
+     var request = $.ajax({
+        url: "/project",
+        type: "POST",
+        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify({ project : project })
+      }).done(function(data) {
+           console.log("project posted!" );
+           window.location.href = 'http://localhost:8042/projects';
+      }).fail(function( data ) {
+           console.log("project up failed" );
+      });
+  });
+
+  $("#favTeacherField").click(function(){
+    $(this).val('Professor Igor Ivkovic');
   });
 
 
