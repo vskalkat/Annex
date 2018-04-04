1f97f3fff3a6beed77decff28b3b30efce3e178a
Saif Khan <>
2018-04-04 06:03:29 -0400

    search

--- server/public/findProjectsView.js
+++ server/public/findProjectsView.js
@@ -17,9 +17,60 @@ $(document).ready(function(){
       	console.log("faaiiluuure");
       });
 
+                    console.log("try2");
+
+    $(document).keyup(function(event) {
+        if (event.key == "Enter") { //$(".searchField").is(":focus") && 
+
+            console.log("try");
+          var endPoint = "/projects";
+          if($("#searchField").val().length > 0) {
+              endPoint = "/search/project/" + $("#searchField").val();
+          }
 
 
-      
+           var request = $.ajax({
+              url: endPoint,
+              type: "GET",
+              dataType: "json",
+              contentType: 'application/json; charset=utf-8',
+              data: {}
+            }).done(function(data) {
+              console.log("data got");
+                $('.projectsList').empty(); 
+              console.log(data);
+              for (var i = data.length - 1; i >= 0; i--) {
+                
+                $('.projectsList').append("<a style='text-decoration:none;' href='#projectModal' rel='modal:open'><li class='list-group-item projectItem'><h3 class='projectTitle'>"+  data[i].title +"</h3><p class='projectDescription'>"+ data[i].description +"</p></li></a>");
+              }
+            }).fail(function( data ) {
+              console.log("faaiiluuure");
+            });
+
+          //   var request = $.ajax({
+          //       url: endPoint,
+          //       type: "GET",
+          //       dataType: "json",
+          //       contentType: 'application/json; charset=utf-8',
+          //       data: {}
+          //     }).done(function(data) {
+          //       console.log("data got");
+          //       console.log(data);
+          //       $('.projectsList').empty(); 
+
+          //       for (var i = data.length - 1; i >= 0; i--) {
+                  
+          //         $('.projectsList').append("<a style='text-decoration:none;' href='#projectModal' rel='modal:open'><li class='list-group-item projectItem'><h3 class='projectTitle'>"+  data[i].title +"</h3><p class='projectDescription'>"+ data[i].description +"</p></li></a>");
+          //       }
+          //     }).fail(function( data ) {
+          //       console.log("faaiiluuure");
+          //     });
+          // }
+
+        }
+    });
+
+
 
 
 });
